cd2b605acddad38c56c4cd6f30413ded8ff7eab0
Zinc <minkevicius.linas@gmail.com>
2019-01-06 12:49:52 +0200

    Pid Activation init

--- Avoidance.ino
+++ Avoidance.ino
@@ -1,10 +1,12 @@
 
-void resetIntegrators() {
+void resetIntegrators()
+{
 
   LWint = 0;
   RWint = 0;
 }
-void StupidAvoidance() {
+void StupidAvoidance()
+{
 
   //  if (ObF || ObL || ObR)
   //  { inX = 0;
@@ -27,7 +29,9 @@ void StupidAvoidance() {
   if (ObF)
   {
     if (distances[0] > distances[1])
-    { for (int i = 0; i < 60; i++) {
+    {
+      for (int i = 0; i < 60; i++)
+      {
         setLeft(-i - 10);
         setRight(-i);
 
@@ -35,31 +39,30 @@ void StupidAvoidance() {
       }
     }
     else
-    { for (int i = 0; i < 60; i++) {
+    {
+      for (int i = 0; i < 60; i++)
+      {
         setLeft(-i);
         setRight(-i - 10);
 
         delay(7);
       }
     }
-
   }
 
   if (!ObF && !ObL && !ObR)
-  { inX = ConstantSpeed;
+  {
+    inX = ConstantSpeed;
     inY = ConstantSpeed;
   }
 
   controlEngines(inX, inY);
-
-
-
 }
-void StupidAvoidancePIF() {
+void StupidAvoidancePIF()
+{
 
   if (ObL)
   {
-
 
     controlEngines(0, 0);
     resetIntegrators();
@@ -70,8 +73,6 @@ void StupidAvoidancePIF() {
       displayData();
       controlEngines(-ConstantSpeed / 2, ConstantSpeed / 2);
     }
-
-
   }
   if (ObR)
   {
@@ -87,29 +88,24 @@ void StupidAvoidancePIF() {
     }
   }
 
-
   if (!ObF && !ObL && !ObR)
-  { inX = ConstantSpeed;
+  {
+    inX = ConstantSpeed;
     inY = ConstantSpeed;
 
     controlEngines(inX, inY);
   }
-
-
-
-
 }
-void StupidAvoidancev3() {
+void StupidAvoidancev3()
+{
   Forward(90);
   if (distances[1] <= 600)
   {
     turnLeft(80);
-
   }
   if (distances[1] <= 400 && distances[0] <= 400)
   {
     turnRight(80);
-
   }
   if (distances[1] <= 600 && distances[2] <= 400)
   {
@@ -121,9 +117,10 @@ void StupidAvoidancev3() {
     setRight(-70);
   }
 }
-void StupidAvoidancev3PID() {
+void StupidAvoidancev3PID()
+{
   ForwardP(ConstantSpeed);
-    TuF = true;
+  TuF = true;
   if (distances[1] <= 300)
   {
     turnLeftP(ConstantSpeed);
@@ -142,69 +139,74 @@ void StupidAvoidancev3PID() {
     TuR = false;
   }
   if (distances[1] <= 300 && distances[0] <= 300 && distances[2] <= 300)
-  { TuL = true;
+  {
+    TuL = true;
     TuR = true;
     //setLeft(-60);
     //setRight(-50);
     turnRightP(ConstantSpeed);
   }
 }
-void AngleAvoidancePID(){
-  
-    controlEngines(ConstantSpeed*!ObF+ConstantSpeed*cos(Rangle), 
-    ConstantSpeed*!ObF+ConstantSpeed*cos(Langle));
+void AngleAvoidancePID()
+{
+
+  controlEngines(ConstantSpeed * !ObF + ConstantSpeed * cos(Rangle),
+                 ConstantSpeed * !ObF + ConstantSpeed * cos(Langle));
 }
-void DistanceAvoidancePID(){
-  
-    controlEngines(ConstantSpeed*!ObF+ConstantSpeed*cos(Rangle), 
-    ConstantSpeed*!ObF+ConstantSpeed*cos(Langle));
+void DistanceAvoidancePID()
+{
+
+  controlEngines(ConstantSpeed * !ObF + ConstantSpeed * cos(Rangle),
+                 ConstantSpeed * !ObF + ConstantSpeed * cos(Langle));
 }
 
-
-
-
-float controlLeftDistance(float L) {
+float controlLeftDistance(float L)
+{
   float goal = 0;
-
 
   float error = L - distances[0];
   L_int += error;
-  L_dif = error - L_Last_e ;
+  L_dif = error - L_Last_e;
   L_Last_e = error;
   goal = LKp * error + LKi * L_int + LKd * L_dif;
   //goal -= controlFrontDistance(D_Setpoint);
   goal = constrain(goal, -SpeedLimit, SpeedLimit);
 
   return goal;
-
 }
 
-float controlRightDistance(float R) {
+float controlRightDistance(float R)
+{
   float goal = 0;
-
 
   float error = R - distances[2];
   R_int += error;
-  R_dif =  error - R_Last_e;
+  R_dif = error - R_Last_e;
   R_Last_e = error;
   goal = RKp * error + RKi * R_int + RKd * R_dif;
   //goal -= controlFrontDistance(D_Setpoint);
   goal = constrain(goal, -SpeedLimit, SpeedLimit);
 
   return goal;
-
 }
-float controlFrontDistance(float F) {
+float controlFrontDistance(float F)
+{
   float goal = 0;
 
   float error = F - distances[1];
   Dint += error;
-  Ddif =  error - D_Last_e ;
+  Ddif = error - D_Last_e;
   D_Last_e = error;
   goal = DKp * error + DKi * Dint + DKd * Ddif;
 
   goal = constrain(goal, -SpeedLimit, SpeedLimit);
 
   return goal;
-
+}
+void ActivatePID()
+{
+  float Fspeed, Lspeed, Rspeed = 0;
+  Fspeed = controlFrontDistance(distances[1]);
+  Lspeed = controlLeftDistance(distances[0]);
+  Rspeed = controlRightDistance(distances[2]);
 }
